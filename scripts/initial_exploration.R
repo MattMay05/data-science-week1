2b951d962046bee3a2cc220e4ff7b7e115ca4259
MattMay05 <yya23nqu@uea.ac.uk>
2026-02-05 20:16:51 +0000

    Created Week 2 cleaning plan

--- scripts/initial_exploration.R
+++ scripts/initial_exploration.R
@@ -40,3 +40,68 @@ mosquito_egg_raw |>
 #   Female 23 laid less than hatched
 # - Any obvious problems?
 #   Missing values in body mass, eggs laid, and eggs hatched
+
+mosquito_egg_raw |>
+  distinct(site)
+
+str_c("Site", "B", sep = "_")
+str_c("Site", "C", sep = "_")
+str_c("Site", "A", sep = "_")
+
+mosquito_egg_raw |>
+  distinct(site)
+
+mosquito_egg_raw |>
+  distinct(collector)
+
+mosquito_egg_raw |>
+  distinct(treatment)
+
+mosquito_egg_clean <- mosquito_egg_raw |>
+  mutate(site = case_when(
+    site == "Site B" ~ "Site_B",
+    site == "Site A" ~ "Site_A",
+    site == "Site C" ~ "Site_C",
+    site == "Site-C" ~ "Site_C",
+    site == "Site-B" ~ "Site_B",
+    site == "Site-A" ~ "Site_A",
+    site == "site c" ~ "Site_C",
+    site == "site b" ~ "Site_B",
+    site == "site a" ~ "Site_A",
+    .default = as.character(site) 
+  )
+  ) 
+
+mosquito_egg_clean |>
+  distinct(site)
+
+mosquito_egg_clean <- mosquito_egg_raw |> 
+  mutate(site = stringr::str_to_upper(site))
+
+mosquito_egg_clean |>
+  distinct(site)
+
+mosquito_egg_raw |> 
+  filter(duplicated(across(everything())))
+sum()
+
+mosquito_egg_raw |> 
+  summarise(
+    n = n(),
+    n_distinct(female_id)
+  )
+
+mosquito_egg_raw |> 
+  summarise(
+    n = n(),
+    n_distinct(collection_date)
+  )
+
+library(skimr)
+skimr::skim(mosquito_egg_raw)
+
+# Check ranges of all numeric variables at once
+mosquito_egg_raw |> 
+  summarise(across(where(is.numeric), 
+                   list(min = ~min(., na.rm = TRUE),
+                        max = ~max(., na.rm = TRUE))))            
